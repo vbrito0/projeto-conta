c5422f00ac51957627ae3903e4fc231a727a1875
Rafael <rlgonzalez@apoioprodesp.sp.gov.br>
2022-09-09 15:56:26 -0300

    Date Select

--- backend/src/main/java/com/devsuperior/dsmeta/repositories/VendasRepository.java
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/VendasRepository.java
@@ -1,9 +1,16 @@
 package com.devsuperior.dsmeta.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dsmeta.entities.Vendas;
 
 public interface VendasRepository extends JpaRepository<Vendas, Long>{
 	
+	@Query("SELECT obj FROM Venda obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+	Page<Vendas> findVendas(LocalDate min, LocalDate max, Pageable pageable);
 }
--- backend/src/main/java/com/devsuperior/dsmeta/services/VendasService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/VendasService.java
@@ -1,8 +1,12 @@
 package com.devsuperior.dsmeta.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.entities.Vendas;
@@ -14,7 +18,13 @@ public class VendasService {
 	@Autowired
 	private VendasRepository vendasRepo;
 
-	public List<Vendas> findVendas() {
-		return vendasRepo.findAll();
+	public Page<Vendas> findVendas(String minDate, String maxDate, Pageable pageable) {
+		
+		LocalDate hoje = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min = minDate.equals("") ? hoje.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? hoje : LocalDate.parse(maxDate);
+		
+		return vendasRepo.findVendas(min, max, pageable);
 	}
 }
