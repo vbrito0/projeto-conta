6172eeea9b8225ce05ea58f1763d8351f5dc6c3d
Rafael <rlgonzalez@apoioprodesp.sp.gov.br>
2022-09-09 00:21:23 -0300

    Project clean

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,11 +1,11 @@
-import icon from '../../../src/assets/img/notification-icon.svg'
+import icon from '../../src/assets/img/notification-icon.svg'
 
 import './styles.css'
 
 function NotificationButton(){
     return(
         <div className="dsmeta-red-btn">
-            <img src="{icon}" alt="Notificar" />
+            <img src={icon} alt="Notificar" />
         </div>
     )
 }
